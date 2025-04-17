d684856554e3fc86b5645556caf59a0fb7fb016f
ramtin-ab <abdolhoseiniramtin@gmail.com>
2025-04-17 12:46:18 +0330

    fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,11 +1,8 @@
 # 0,1, 1, 2, 3, 5, 8, 13 ,21,34,45,89
-#shomare jomle
 j=[0,1,1]
 i=3
 while 3<=i<100:
     j.append(j[i-1]+j[i-2])
     i+=1
 number=int(input("enter number of fibonnaci sequence :"))
-print(j[number-1])
-#s5 e2 reza dolati 
-#ravesh sadetar dare
+print(j[number-1]) 
